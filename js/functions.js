a4019a8ff845af281549cf26d141f3383f75aedf
helga2lip <helga2lip@gmail.com>
2023-10-19 19:10:04 +0300

    домашнее задание 2 раздел

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,19 @@
-console.log('Hello, world');
 const isStringValid = (string, maxLength) => string.length <= maxLength;
-// if (string.length <= maxLength) {
-//   return true;
-// }
-// return false;
 
 console.log(isStringValid('проверяемая строка', 20));
 console.log(isStringValid('проверяемая строка', 18));
 console.log(isStringValid('проверяемая строка', 10));
 
-const isPalindrom = (string) => {
+const isPalindrome = (string) => {
+  const normalisedString = string.replaceAll(' ', '').toLowerCase();
   let newString = '';
-  for (let i = string.length - 1; i >= 0; i--) {
-    newString = string.at(i);
+  for (let i = normalisedString.length - 1; i >= 0; i--) {
+    newString += normalisedString.at(i);
   }
-  console.log(newString);
-}
+  console.log(normalisedString, newString);
+  return normalisedString === newString;
+};
 
-isPalindrom('qwerty');
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('До вОд'));
+console.log(isPalindrome('Кекс'));
