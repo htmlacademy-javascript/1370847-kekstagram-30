6a054249ecf93d843690387b4aaddf183bb67f98
helga2lip <helga2lip@gmail.com>
2023-10-26 19:52:33 +0300

    фукция создает массив

--- js/main.js
+++ js/main.js
@@ -19,3 +19,17 @@ const getRandomInteger = (a, b) => {
 };
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const generatePhotosArray = (count) => {
+  const photosArray = [];
+  for (let i = 0; i < count; i++) {
+    const photo = {
+      id: i + 1,
+      name: 'gvgvg',
+    };
+    photosArray.push(photo);
+  };
+  return photosArray;
+};
+
+console.log(generatePhotosArray(3));
