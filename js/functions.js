7ce986a7229a295494369ddc46e5c808b0a2e233
helga2lip <helga2lip@gmail.com>
2023-11-11 18:55:27 +0300

    убрала ошибки в functions

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,19 @@
-const isStringValid = (string, maxLength) => string.length <= maxLength;
+export const isStringValid = (string, maxLength) => string.length <= maxLength;
 
-console.log(isStringValid('проверяемая строка', 20));
-console.log(isStringValid('проверяемая строка', 18));
-console.log(isStringValid('проверяемая строка', 10));
+//console.log(isStringValid('проверяемая строка', 20));
+//console.log(isStringValid('проверяемая строка', 18));
+//console.log(isStringValid('проверяемая строка', 10));
 
-const isPalindrome = (string) => {
+export const isPalindrome = (string) => {
   const normalisedString = string.replaceAll(' ', '').toLowerCase();
   let newString = '';
   for (let i = normalisedString.length - 1; i >= 0; i--) {
     newString += normalisedString.at(i);
   }
-  console.log(normalisedString, newString);
+  //console.log(normalisedString, newString);
   return normalisedString === newString;
 };
 
-console.log(isPalindrome('топот'));
-console.log(isPalindrome('До вОд'));
-console.log(isPalindrome('Кекс'));
+//console.log(isPalindrome('топот'));
+//console.log(isPalindrome('До вОд'));
+//console.log(isPalindrome('Кекс'));
